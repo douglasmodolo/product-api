f39a27926b06cc4c40751c37137b0b46a5d70d78
Douglas H. Módolo <66284042+douglasmodolo@users.noreply.github.com>
2025-07-12 17:09:03 -0300

    Created AppDbContext (#2)

---
+++ WebAPI/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional model configuration can go here
+        }        
+    }
+}
