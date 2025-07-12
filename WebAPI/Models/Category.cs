718c913017039667433255b2adf31b79287321d9
Douglas Modolo <douglasmodolo@outlook.com.br>
2025-07-12 16:34:55 -0300

    Create Product and Category Models

---
+++ WebAPI/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? UrlImage { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ WebAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? UrlImage { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; } // Navigation property to Category
+    }
+}
