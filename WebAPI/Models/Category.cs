6f7b285a5da381715256acbb5ae5eadf2902e4e4
Douglas H. Módolo <66284042+douglasmodolo@users.noreply.github.com>
2025-07-12 20:04:38 -0300

    Insert data annotations and create migrations (#5)

--- WebAPI/Models/Category.cs
+++ WebAPI/Models/Category.cs
@@ -1,12 +1,29 @@
-﻿namespace WebAPI.Models
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
 {
+    [Table("Categories")]
     public class Category
     {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(80, ErrorMessage = "O nome deve ter entre 1 e 80 caracteres", MinimumLength = 1)]
         public string? Name { get; set; }
+
+        [StringLength(300, ErrorMessage = "A descrição deve ter entre 1 e 300 caracteres", MinimumLength = 1)]
         public string? UrlImage { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public ICollection<Product>? Products { get; set; }
+
+        public DateTime CreatedAt { get; set; }        
+        public DateTime UpdatedAt { get; set; }        
+        public ICollection<Product>? Products { get; set; }        
     }
 }
--- WebAPI/Models/Product.cs
+++ WebAPI/Models/Product.cs
@@ -1,16 +1,30 @@
-﻿namespace WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
 {
+    [Table("Products")]
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(80, ErrorMessage = "O nome deve ter entre 1 e 80 caracteres", MinimumLength = 1)]
         public string? Name { get; set; }
+
+        [StringLength(300, ErrorMessage = "A descrição deve ter entre 1 e 300 caracteres", MinimumLength = 1)]
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "O preço é obrigatório")]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Price { get; set; }
-        public string? UrlImage { get; set; }
-        public float Stock { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public int CategoryId { get; set; }
+        
+        public string? UrlImage { get; set; }        
+        public float Stock { get; set; }        
+        public DateTime CreatedAt { get; set; }        
+        public DateTime UpdatedAt { get; set; }        
+        public int CategoryId { get; set; }        
         public Category? Category { get; set; } // Navigation property to Category
     }
 }
