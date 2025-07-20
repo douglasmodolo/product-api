daedac1d6aa1a8041edc052deaa8832a2222ef9b
Douglas Modolo <douglasmodolo@outlook.com.br>
2025-07-20 19:42:41 -0300

    create middleware

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Context;
+using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ConfigureExceptionHandler(); // Use custom exception handler in development
 }
 
 app.UseHttpsRedirection();
