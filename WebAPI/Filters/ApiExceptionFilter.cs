6aa42c577968fbe5cee148a67c853c0a90531e6d
Douglas H. Módolo <66284042+douglasmodolo@users.noreply.github.com>
2025-07-21 20:54:55 -0300

    exception filter created (#13)

---
+++ WebAPI/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebAPI.Filters
+{
+    public class ApiExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<ApiExceptionFilter> _logger;
+
+        public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception, "An unhandled exception occurred while processing the request.");
+
+            context.Result = new ObjectResult(new
+            {
+                Error = "An unexpected error occurred. Please try again later.",
+                Details = context.Exception.Message
+            })
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+}
